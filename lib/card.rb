69d3d8295ce8f2f73e4cd2fa44928c02aeb14cca
Ben Blanchard <benjamin.blanchard@gmail.com>
2017-10-28 15:20:48 -0400

    Passed jack test.

--- lib/card.rb
+++ lib/card.rb
@@ -3,6 +3,13 @@ class Card
     attr_reader :rank
     def initialize(suit:, rank:)
         @suit = suit
-        @rank = rank
+        @rank = case rank
+          when :jack then 11
+          when :queen then 12
+          when :king then 13
+          else rank
+        end
     end
+
+
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe 'a playing card' do
    it 'has a rank' do
      raise unless Card.new(suit: :spades, rank: 4).rank == 4
    end
-   
+
    describe 'a Jack' do
       it 'ranks higher than a 10' do
          a_jack = Card.new(suit: :spades, rank: :jack)
-         a_ten = Card.new(suit: :spades, rank: :jack)
+         a_ten = Card.new(suit: :spades, rank: 10)
          raise unless a_jack.rank > a_ten.rank
       end
    end
-   
+
 end
