492e41c668ebd0a90a144a05cb8e3fa9886c6825
Ben Blanchard <benjamin.blanchard@gmail.com>
2017-10-28 14:58:39 -0400

    Added rank to pass rank test.

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,8 @@
 class Card
     attr_reader :suit
+    attr_reader :rank
     def initialize(suit:, rank:)
         @suit = suit
+        @rank = rank
     end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe 'a playing card' do
    it 'has a suit' do
       raise unless Card.new(suit: :spades, rank: 4).suit == :spades
    end
+
+   it 'has a rank' do
+     raise unless Card.new(suit: :spades, rank: 4).rank == 4
+   end
+   
 end
