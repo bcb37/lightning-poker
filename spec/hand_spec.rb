338134b2c430b78e7e26167d8ac3960fd1a20281
Ben Blanchard <benjamin.blanchard@gmail.com>
2017-10-29 16:12:47 -0400

    Added failure message.

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -5,6 +5,10 @@ describe 'hand_rankings' do
         match do |winning|
             HighCard.beats?(hand(winning), hand(losing))
         end
+
+        failure_message do |winning|
+          "expected <#{hand(winning).join(" ")}> to beat <#{hand(losing).join(" ")}>"
+        end
     end
     def hand(strings)
         strings.map{|x| Card.from_string(x)}
