146bebce24ff94377a62c984cf7e81c9cc2a67fd
Ben Blanchard <benjamin.blanchard@gmail.com>
2017-10-30 14:10:31 +0000

    Add and test beats method

--- lib/high_card.rb
+++ lib/high_card.rb
@@ -2,6 +2,10 @@ require 'card'
 
 module HighCard
     def self.beats?(hand, opposing)
-        hand.map(&:rank).sort.last > opposing.map(&:rank).sort.last
+        if (hand.map(&:rank).sort.last == opposing.map(&:rank).sort.last)
+            return hand.map(&:rank).sort[-2] > opposing.map(&:rank).sort[-2]
+        else
+            return hand.map(&:rank).sort.last > opposing.map(&:rank).sort.last
+        end
     end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,6 +1,6 @@
 require 'high_card'
 
-describe 'hand_rankings' do
+describe 'hand_rankings', :aggregate_failures do
     matcher :beat do |losing|
         match do |winning|
             HighCard.beats?(hand(winning), hand(losing))
@@ -18,5 +18,10 @@ describe 'hand_rankings' do
         expect(%w(7H)).to_not beat(%w(8D))
         expect(%w(7H QD)).to beat(%w(6D JC))
         expect(%w(7H JC)).to_not beat(%w(6D QS))
+    end
+    
+    example 'next highest card is used as tie-breaker' do
+        expect(%w(7H JC)).to beat(%w(6D JS))
+        expect(%w(7H JC)).to_not beat(%w(8D JS))
     end
 end
